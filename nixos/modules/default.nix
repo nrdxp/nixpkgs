67c1551117070aa6650b795db3cadf27c31f7a89
Timothy DeHerrera <tim.deh@pm.me>
2021-12-11 20:22:23 -0700

    modules: export camel-cased

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,6 +1,21 @@
+with builtins;
 let
+  toCamelCase = string:
+    let
+      toUpper = string:
+        let
+          lowerChars = [ "a" "b" "c" "d" "e" "f" "g" "h" "i" "j " "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ];
+          upperChars = [ "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ];
+        in
+        replaceStrings lowerChars upperChars string;
+      words = filter (word: isString word) (split "-|_" string);
+      camels = concatMap
+        (word: [ (toUpper (head word)) ] ++ (tail word))
+        (map (string: match "(^.)(.*)" string) (tail words));
+    in
+    concatStringsSep "" ([ (head words) ] ++ camels);
   moduleList = import ./module-list.nix;
-  modules = with builtins;
+  modules =
     let
       modules' = map
         (path:
@@ -8,13 +23,15 @@ let
             name' = baseNameOf path;
             len = stringLength name';
             # remove file suffix
-            name = substring 0 (len - 4) name';
+            name = toCamelCase (substring 0 (len - 4) name');
           in
           { inherit name; value = import path; })
         moduleList;
     in
     listToAttrs modules';
 in
-modules // {
+modules //
+  # modules that shouldn't be included as part of the base system go here
+{
   notDetected = import ./installer/scan/not-detected.nix;
 }
